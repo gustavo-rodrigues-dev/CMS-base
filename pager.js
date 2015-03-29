7edd39886524b6bf2a61966203191ae6c5afadf6
Gustavo da Silva Rodrigues <gustavo.beavis@gmail.com>
2015-03-29 20:29:32 -0300

    atualizada imagens das paginas

--- pager.js
+++ pager.js
@@ -8,6 +8,7 @@ var pageres = new Pageres({
     .src('http://localhost:9000/admListaMateria.html', ['360x640','640x360', '800x600' ,'1024x768'], {crop: false})
     .src('http://localhost:9000/admListarCategoria.html', ['360x640','640x360', '800x600' ,'1024x768'], {crop: false})
     .src('http://localhost:9000/admListarMensagens.html', ['360x640','640x360', '800x600' ,'1024x768'], {crop: false})
+    .src('http://localhost:9000/addMateria.html', ['360x640','640x360', '800x600' ,'1024x768'], {crop: false})
     .dest('./screenshots/');
 
 
